40f1859524919498c344d8cf29421ac9fd63f9ac
rfnsilva <rfnsbadoo@gmail.com>
2021-02-02 08:40:23 -0300

    ajustes

--- mongodb-project/client/src/config/firebase.ts
+++ mongodb-project/client/src/config/firebase.ts
@@ -2,13 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCI5cpheURFtIT8gY5OcNcYwZ3G_K2Ss_Q',
-  authDomain: 'preparo-8836b.firebaseapp.com',
-  projectId: 'preparo-8836b',
-  storageBucket: 'preparo-8836b.appspot.com',
-  messagingSenderId: '220285738626',
-  appId: '1:220285738626:web:90a61a066e7e255531de9b',
-  measurementId: 'G-SWC8F1277Q'
+  apiKey: 'AIzaSyD0BaSwx75B6Il9KJKcUPvua0PCRjjmMqo',
+  authDomain: 'my-preparo.firebaseapp.com',
+  projectId: 'my-preparo',
+  storageBucket: 'my-preparo.appspot.com',
+  messagingSenderId: '191883021217',
+  appId: '1:191883021217:web:24815077192ce1c9ce19e0'
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
